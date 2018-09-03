0fccfe579fa621ef59a9349a01e95e995dbff1dc
amitatatatata <popopo.badbad@docomo.ne.jp>
2018-09-03 18:35:50 +0900

    修造をImageからSpriteに変更した

--- Assets/Scripts/AmiScene/ShuzoManager.cs
+++ Assets/Scripts/AmiScene/ShuzoManager.cs
@@ -7,10 +7,12 @@ public class ShuzoManager : MonoBehaviour {
     [SerializeField] GameObject frameImage;
 
     Animator animator;
+    Rigidbody2D rBody;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+        rBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -28,5 +30,10 @@ public class ShuzoManager : MonoBehaviour {
     {
         animator.speed = 1.0f;
         frameImage.SetActive(false);
+    }
+
+    public void Jump()
+    {
+        rBody.AddForce(new Vector2(0.0f, 30.0f));
     }
 }
