4b9b5464a025493ac40f75be71b59c90facfd9a2
amitatatatata <popopo.badbad@docomo.ne.jp>
2018-09-04 17:07:33 +0900

    Add RacketItem.

--- Assets/Scripts/AmiScene/ShuzoManager.cs
+++ Assets/Scripts/AmiScene/ShuzoManager.cs
@@ -7,6 +7,7 @@ public class ShuzoManager : MonoBehaviour {
     [SerializeField] private GameObject frameImage;     //炎演出用Image。 SetActiveで表示、非表示を切り替える
     [SerializeField] private Camera camera;             //プレイヤーを追うカメラ
     [SerializeField] private LayerMask groundLayer;     //地面のレイヤー
+    [SerializeField] private float speedX = 15.0f;
 
     private Animator animator;
     private Rigidbody2D rBody;
@@ -14,14 +15,14 @@ public class ShuzoManager : MonoBehaviour {
     //trueの時にはジャンプできず、毎フレーム着地したか確認する。
     bool isJump = true;
 
-    //ジャンプ後数フレームは地面に触れたままなので、10フレームはジャンプ負荷にする
-    //そのためにフレーム数を数える変数
-    int frames = 0;
+    //元のスピード
+    private float baseSpeedX;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         rBody = GetComponent<Rigidbody2D>();
+        baseSpeedX = speedX;
 	}
 	
 	// Update is called once per frame
@@ -32,7 +33,7 @@ public class ShuzoManager : MonoBehaviour {
     void FixedUpdate()
     {
         //プレイヤーを一定速度で常に右に移動させる。
-        rBody.velocity = new Vector2(10.0f, rBody.velocity.y);
+        rBody.velocity = new Vector2(speedX, rBody.velocity.y);
 
         //プレイヤーをカメラの真ん中に常に捉える。
         camera.transform.position = new Vector3(transform.position.x, camera.transform.position.y, camera.transform.position.z);
@@ -41,7 +42,7 @@ public class ShuzoManager : MonoBehaviour {
         if (!isJump && Input.GetButtonDown("Jump")) Jump();
 
         //ジャンプ中の時、地面に着地したか確認する
-        if (isJump && frames > 5)
+        if (isJump && rBody.velocity.y < 0)
         {
             //プレイヤーから二本の線を下に飛ばして地面に触れていたら着地している(isJump = false)とする。
             isJump = !(Physics2D.Linecast(transform.position + (Vector3.left * 0.3f),
@@ -50,7 +51,7 @@ public class ShuzoManager : MonoBehaviour {
                 transform.position + (Vector3.down * 0.1f), groundLayer)
                 );
         }
-        else if (frames <= 5) frames++;
+        
     }
 
     //炎演出を表示させる。
@@ -58,6 +59,7 @@ public class ShuzoManager : MonoBehaviour {
     {
         //プレイヤーの動きを激しくする。
         animator.speed = 2.0f;
+        speedX *= 2;
         //炎をみえるようにする。
         frameImage.SetActive(true);
     }
@@ -67,6 +69,7 @@ public class ShuzoManager : MonoBehaviour {
     {
         //プレイヤーの動きを元に戻す。
         animator.speed = 1.0f;
+        speedX = baseSpeedX;
         //炎を見えなくする。
         frameImage.SetActive(false);
     }
@@ -78,16 +81,22 @@ public class ShuzoManager : MonoBehaviour {
 
         //Jump中にする。
         isJump = true;
-        frames = 0;
     }
 
-    //プレイヤーがゴールに触れたか判定する。
+    
     void OnTriggerEnter2D(Collider2D col)
     {
         //もし触れたものがGoalタグならプレイヤーの位置を元に戻す。
         if(col.gameObject.tag == "Goal")
         {
             transform.position = new Vector3(0, 0, 0);
+        }
+
+        if(col.gameObject.tag == "Racket")
+        {
+            Destroy(col.gameObject);
+            OnFrameMode();
+            Invoke("OffFrameMode", 1.0f);
         }
     }
 }
