d95ac9dc6b36a4c33db7e3d898ea77fbfe941af4
amitatatatata <popopo.badbad@docomo.ne.jp>
2018-09-04 14:39:45 +0900

    Add Jump and Goal.

--- Assets/Scripts/AmiScene/ShuzoManager.cs
+++ Assets/Scripts/AmiScene/ShuzoManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShuzoManager : MonoBehaviour {
-    [SerializeField] GameObject frameImage;
+    [SerializeField] private GameObject frameImage;     //炎演出用Image。 SetActiveで表示、非表示を切り替える
+    [SerializeField] private Camera camera;             //プレイヤーを追うカメラ
+    [SerializeField] private LayerMask groundLayer;     //地面のレイヤー
 
-    Animator animator;
-    Rigidbody2D rBody;
+    private Animator animator;
+    private Rigidbody2D rBody;
+
+    //trueの時にはジャンプできず、毎フレーム着地したか確認する。
+    bool isJump = true;
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,23 +23,69 @@ public class ShuzoManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
 
+    void FixedUpdate()
+    {
+        Debug.Log(isJump);
+        //プレイヤーを一定速度で常に右に移動させる。
+        rBody.velocity = new Vector2(10.0f, rBody.velocity.y);
+
+        //プレイヤーをカメラの真ん中に常に捉える。
+        camera.transform.position = new Vector3(transform.position.x, camera.transform.position.y, camera.transform.position.z);
+
+        //Jump対応キーを押したらジャンプする。
+        if (Input.GetButtonDown("Jump")) Jump();
+
+        //ジャンプ中の時、地面に着地したか確認する
+        if (isJump)
+        {
+            //プレイヤーから二本の線を下に飛ばして地面に触れていたら着地している(isJump = false)とする。
+            isJump = !(Physics2D.Linecast(transform.position + (Vector3.left * 0.3f),
+                transform.position + (Vector3.down * 0.2f), groundLayer) ||
+                    Physics2D.Linecast(transform.position + (Vector3.right * 0.3f),
+                transform.position + (Vector3.down * 0.2f), groundLayer)
+                );
+        }
+    }
+
+    //炎演出を表示させる。
     public void OnFrameMode()
     {
+        //プレイヤーの動きを激しくする。
         animator.speed = 2.0f;
+        //炎をみえるようにする。
         frameImage.SetActive(true);
     }
 
+    //炎演出を非表示にする。
     public void OffFrameMode()
     {
+        //プレイヤーの動きを元に戻す。
         animator.speed = 1.0f;
+        //炎を見えなくする。
         frameImage.SetActive(false);
     }
 
     public void Jump()
     {
-        rBody.AddForce(new Vector2(0.0f, 30.0f));
+        if (isJump) return; 
+
+        //rigidbodyに上向きの力を加えてジャンプさせる。
+        rBody.AddForce(new Vector2(0.0f, 300.0f));
+
+        //Jump中にする。
+        isJump = true;
+    }
+
+    //プレイヤーがゴールに触れたか判定する。
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //もし触れたものがGoalタグならプレイヤーの位置を元に戻す。
+        if(col.gameObject.tag == "Goal")
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
     }
 }
